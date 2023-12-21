7a5d2b6a46954c412ab6d8416ee569671f07f20c
goshva <sha.egor@gmail.com>
2023-12-21 20:37:07 +0300

    make simple gen to docs folder

--- docs/js/script.js
+++ docs/js/script.js
@@ -1298,5 +1298,41 @@
 			}
 		}
 
-	});
+
+		function feedback(action, status) {
+			//
+			let token =
+			  "5430048154:AAEFptLp8IdbKirOYJzzM3ekyTd2ibVLMNc"; /* :TODO NOTSECURITY REWERITE */
+			//use this for testing
+			let chat_id = "190404167";
+			//let chat_id = "1329475336";//Aram ID
+			//use this for production
+			//let chat_id = "-915348868";
+			let user_phone = prompt("Введите телефон чтоб уточнить удобное время,🙏 ");
+			var msg = `${action} от ${user_phone}`; // from ${getCookie("@")}`;
+			var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}&parse_mode=html`;
+		  console.log(url)
+			if (user_phone !== "" && user_phone !== null) {
+			  fetch(url)
+				.then((response) => {
+				  return response.json();
+				})
+				.then((data) => {
+				  alert(
+					`Благодарим, стоимость билета 1000р \n Cейчас перезвоним` // «${action}».  Сейчас оплата запустится`
+				  );
+				  window.location.href = "/#menu";
+				});
+			} else {
+			  console.log(" no null");
+			}
+		  }
+		  
+		  $('.lead').click(
+			  function () { this.addEventListener("click", feedback(this.innerHTML)) }
+			);
+		});
+
+
+
 }());
